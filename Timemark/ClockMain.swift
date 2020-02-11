e4d2cfa306584d8a529a616e9b1099aa06caad10
PepperLola <julian.orion.wright@gmail.com>
2020-02-10 19:14:24 -0800

    haptics work, the seconds hand gets set to the right value when the view loads
    this is important to the user experience because it allows the user to tell the seconds. this allows the user to be more precise with their time telling, and being precise is always (usually) a good thing. this adds positively to the user experience, because even though it's another thing (more clutter), it's relatively unintrusive, meaning we've stuck to the minimalistic design, while adding more information. this is, if you ask me, quite a feat, considering that adding more information generally has the tendency to increase clutter and *detract* from the user experience. if only markdown worked in commit messages.

--- Timemark/ClockMain.swift
+++ Timemark/ClockMain.swift
@@ -15,32 +15,51 @@ class ClockViewController: UIViewController {
     
     let clock = Clock()
     
+    let timeFormatter = DateFormatter()
+    let dateFormatter = DateFormatter()
+    let secondsFormatter = DateFormatter()
+    
     @IBOutlet weak var secondsHandImage: UIImageView!
     @IBOutlet weak var setTime: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
+    let impact = UIImpactFeedbackGenerator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         time = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(DateAndTime), userInfo: nil, repeats: true)
+        
+        timeFormatter.timeStyle = .short
+        dateFormatter.dateStyle = .medium
+        secondsFormatter.dateFormat = "ss"
+        
+        formatTime(absoluteAngle: true)
     }
 
     @objc func DateAndTime () {
-        let time = DateFormatter()
-        let date = DateFormatter()
-        let secondsFormatter = DateFormatter()
+        formatTime(absoluteAngle: false)
+    }
+    
+    func formatTime(absoluteAngle: Bool) {
         
-        time.timeStyle = .short
-        date.dateStyle = .medium
-        secondsFormatter.dateFormat = "ss"
+        setTime.text = timeFormatter.string(from: Date())
         
-        setTime.text = time.string(from: Date())
-        
-        dateLabel.text = date.string(from: Date())
+        dateLabel.text = dateFormatter.string(from: Date())
         
         let seconds = Double(secondsFormatter.string(from: Date()))
         
-        let angle = (2 * Double.pi) / 60
+        var angle = 0.0
+        
+        if (absoluteAngle) {
+            angle = (2 * Double.pi) * (seconds! / 60)
+        } else {
+            angle = (2 * Double.pi) / 60
+        }
         
         secondsHandImage.transform = secondsHandImage.transform.rotated(by: CGFloat(angle))
+        
+        if (seconds == 0) {
+            impact.impactOccurred() //haptics every minute
+        }
     }
 }
