080e82af84af90849a7d4a99b44193df41659b47
Zeqe G <zeqe.g@icloud.com>
2020-02-11 18:45:12 -0800

    metronome ui

---
+++ Timemark/MetronomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MetronomeViewController.swift
+//  Timemark
+//
+//  Created by Zeqiel Golomb on 2/11/20.
+//  Copyright © 2020 Zeqe Golomb. All rights reserved.
+//
+
+import UIKit
+
+class MetronomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+  
+}
