b41e135d1ffe558b67ebe1b30b9c28dd9cff1382
Zeqe G <zeqe.g@icloud.com>
2020-03-22 12:57:46 -0700

    fixed app icons and options

---
+++ Timemark/AppIconsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AppIconsViewController.swift
+//  Timemark
+//
+//  Created by Zeqiel Golomb on 3/22/20.
+//  Copyright © 2020 Zeqe Golomb. All rights reserved.
+//
+
+import UIKit
+
+class AppIconsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   @IBAction func didTapLightIcon(_ sender: UIButton) {
+        doHaptic()
+        UIApplication.shared.setAlternateIconName("purpleicon")
+    }
+    
+    @IBAction func didTapDarkIcon(_ sender: UIButton) {
+        doHaptic()
+        UIApplication.shared.setAlternateIconName("darkicon")
+    }
+    @IBAction func didTapPurpleIcon(_ sender: UIButton) {
+        doHaptic()
+        UIApplication.shared.setAlternateIconName(nil)
+    }
+    @IBAction func didTapShadowIcon(_ sender: UIButton) {
+        doHaptic()
+        UIApplication.shared.setAlternateIconName("shadowicon")
+    }
+    @IBAction func didTapFadeIcon(_ sender: UIButton) {
+        doHaptic()
+        UIApplication.shared.setAlternateIconName("fadeicon")
+    }
+    @IBAction func didTapGlyphIcon(_ sender: UIButton) {
+        doHaptic()
+        UIApplication.shared.setAlternateIconName("glyphicon")
+    }
+
+}
--- Timemark/OptionsViewController.swift
+++ Timemark/OptionsViewController.swift
@@ -13,47 +13,10 @@ class OptionsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Hide tabBar shadow
-        self.tabBarController!.tabBar.layer.borderWidth = 0.50
-        self.tabBarController!.tabBar.layer.borderColor = UIColor.clear.cgColor
-        self.tabBarController?.tabBar.clipsToBounds = true
         
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func didTapLightIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName("purpleicon")
-    }
-    
-    @IBAction func didTapDarkIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName("darkicon")
-    }
-    @IBAction func didTapPurpleIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName(nil)
-    }
-    @IBAction func didTapShadowIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName("shadowicon")
-    }
-    @IBAction func didTapFadeIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName("fadeicon")
-    }
-    @IBAction func didTapGlyphIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName("glyphicon")
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
