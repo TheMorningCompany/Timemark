d6ac18a4f4526df4f173de7602311aaf7a985080
Zeqe G <zeqe.g@icloud.com>
2020-03-29 23:11:11 -0700

    navigation controller for options to reduce the amount of stacked moda views

--- Timemark/OptionsViewController.swift
+++ Timemark/OptionsViewController.swift
@@ -13,7 +13,11 @@ class OptionsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        self.navigationController!.navigationBar.layer.borderWidth = 0.50
+        self.navigationController!.navigationBar.layer.borderColor = UIColor.clear.cgColor
+        self.navigationController?.navigationBar.clipsToBounds = true
+        let backBarButtton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = backBarButtton
         // Do any additional setup after loading the view.
     }
     
