7ca6ade06eabc5249ce3069ac0fd27349ff897e0
Zeqe G <zeqe.g@icloud.com>
2020-03-31 21:02:48 -0700

    some outlets

---
+++ Timemark/AlarmPickerViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AlarmPickerViewController.swift
+//  Timemark
+//
+//  Created by Zeqiel Golomb on 3/31/20.
+//  Copyright © 2020 Zeqe Golomb. All rights reserved.
+//
+
+import UIKit
+
+class AlarmPickerViewController: UITableViewController {
+
+    @IBOutlet weak var check1: UIImageView!
+    @IBOutlet weak var check2: UIImageView!
+    @IBOutlet weak var check3: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+
+}
